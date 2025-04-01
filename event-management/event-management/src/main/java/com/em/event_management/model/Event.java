295132bfeb609069e2cf83a8733b7a9dafc55b4c
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2025-04-01 19:05:52 +0100

    "Second commit"

--- event-management/event-management/src/main/java/com/em/event_management/model/Event.java
+++ event-management/event-management/src/main/java/com/em/event_management/model/Event.java
@@ -1,4 +1,22 @@
 package com.em.event_management.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Event_ID;
+
+    private String Event_Name;
+
+    private String Event_Description;
+
+    private LocalDate Date;
+
+    private String Venue;
 }
